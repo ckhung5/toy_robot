2f58edd0935cdf9c2d9f93d9b00d21523f43754c
Kok Hung Cheah <kokhungcheah@koks-mbp.home>
2020-03-23 23:27:46 +1100

    add method existing_robot?

--- app/model/board.rb
+++ app/model/board.rb
@@ -13,4 +13,10 @@ class Board
 	def add_robot(robot)
 		@robots << robot
 	end
+
+	def exisiting_robot?(x:, y:)
+		@robots.any? do |robot|
+			robot.x == x && robot.y == y
+		end
+	end
 end
--- spec/model/board_spec.rb
+++ spec/model/board_spec.rb
@@ -12,9 +12,39 @@ RSpec.describe Board do
     before do 
       board.add_robot(robot)
     end
-    
+
     it "should add a new robot" do
       expect(board.robots).to eq([robot])
+    end
+  end
+
+  describe "#existing_robot" do
+    context 'when there is no existing robot' do
+      let(:x) { 1 }
+      let(:y) { 1 }
+      let(:board) { described_class.new }
+      
+      subject { board.exisiting_robot?(x: x, y: y)}
+      it "should return false" do
+        expect(subject).to eq(false)
+      end
+    end
+
+    context 'when there is existing robot' do
+      let(:x) { 1 }
+      let(:y) { 1 }
+      let(:direction) { 'EAST' }
+      let(:robot) { Robot.new(x: x, y: y, direction: direction) }
+      let(:board) { described_class.new }
+      
+      before do
+        board.add_robot(robot)
+      end
+
+      subject { board.exisiting_robot?(x: x, y: y)}
+      it "should return true" do
+        expect(subject).to eq(true)
+      end
     end
   end
 end
