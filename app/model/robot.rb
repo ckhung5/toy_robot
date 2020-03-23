e9d837b51529f4a9ad4a21c28966b28bf5d01fcb
Kok Hung Cheah <kokhungcheah@koks-mbp.home>
2020-03-23 23:20:17 +1100

    Add more test for robot model

--- app/model/robot.rb
+++ app/model/robot.rb
@@ -1,5 +1,5 @@
 class Robot
-	attr_reader :x, :y, :direction
+	attr_accessor :x, :y, :direction
 
 	def initialize(x:, y:, direction:)
 		@x = x
@@ -8,6 +8,45 @@ class Robot
 	end
 
 	def report
-		[x, y, direction]
+		[x, y, direction.upcase]
+	end
+
+	def turn_left
+		@direction = case direction.downcase
+		when 'north'
+			'west'
+		when 'south'
+			'east'
+		when 'east'
+			'north'
+		when 'west'
+			'south'
+		end
+	end
+
+	def turn_right
+		@direction = case direction.downcase
+		when 'north'
+			'east'
+		when 'south'
+			'west'
+		when 'east'
+			'south'
+		when 'west'
+			'north'
+		end
+	end
+
+	def move
+		case direction.downcase
+		when 'north'
+			@y += 1 unless y + 1 > 6
+		when 'south'
+			@y -= 1 unless y - 1 < 0
+		when 'east'
+			@x += 1 unless x + 1 > 6
+		when 'west'
+			@x -= 1	unless x - 1 < 0
+		end
 	end
 end
--- spec/model/robot_spec.rb
+++ spec/model/robot_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Robot do
   describe "#report" do
   	let(:x) { 1 }
   	let(:y) { 1 }
-  	let(:direction) { 'east' }
+  	let(:direction) { 'EAST' }
 
   	subject do 
   		described_class.new(x: x, y: y, direction: direction).report
@@ -13,5 +13,78 @@ RSpec.describe Robot do
     it "should report the robot position" do
       expect(subject).to eq([x, y, direction])
     end
+  end
+
+  describe "#turn_left" do
+  	let(:x) { 1 }
+  	let(:y) { 1 }
+  	let(:direction) { 'EAST' }
+  	let(:new_direction) { 'NORTH' }
+  	let(:robot) { described_class.new(x: x, y: y, direction: direction) }
+  	before do
+  		robot.turn_left
+  	end
+    it "should report the robot position" do
+      expect(robot.report).to eq([x, y, new_direction])
+    end
+  end
+
+  describe "#turn_right" do
+  	let(:x) { 1 }
+  	let(:y) { 1 }
+  	let(:direction) { 'EAST' }
+  	let(:new_direction) { 'SOUTH' }
+  	let(:robot) { described_class.new(x: x, y: y, direction: direction) }
+  	before do 
+  		robot.turn_right
+  	end
+
+    it "should report the robot position" do
+      expect(robot.report).to eq([x, y, new_direction])
+    end
+  end
+
+  describe "#move" do
+  	context 'valid movement' do
+	  	let(:x) { 1 }
+			let(:y) { 1 }
+			let(:direction) { 'EAST' }
+			let(:robot) { described_class.new(x: x, y: y, direction: direction) }
+			before do
+				robot.move
+			end
+
+  		it "should report the robot position" do
+     		expect(robot.report).to eq([x+1, y, direction])
+    	end
+  	end
+
+		context 'invalid movement' do
+			context 'when negative x position value' do
+				let(:x) { 0 }
+				let(:y) { 1 }
+				let(:direction) { 'WEST' }
+				let(:robot) { described_class.new(x: x, y: y, direction: direction) }
+				before do
+					robot.move
+				end
+	  		it "should report the unmoved robot position" do
+	     		expect(robot.report).to eq([x, y, direction])
+	    	end	
+			end
+
+			context 'when negative y position value' do
+				let(:x) { 0 }
+				let(:y) { 0 }
+				let(:direction) { 'SOUTH' }
+				let(:robot) { described_class.new(x: x, y: y, direction: direction) }
+				before do
+					robot.move
+				end
+	  		it "should report the unmoved robot position" do
+	     		expect(robot.report).to eq([x, y, direction])
+	    	end	
+			end
+  	end
   end
 end
