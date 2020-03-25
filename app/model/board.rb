0a8d76868d5c26529708fada9f12b82077d3d8be
Kok Hung Cheah <kokhungcheah@koks-mbp.home>
2020-03-26 01:25:49 +1100

    Add method to find robot

--- app/model/board.rb
+++ app/model/board.rb
@@ -1,29 +1,30 @@
+# frozen_string_literal: true
+
 class Board
+  SIZE = 6
 
-	SIZE = 6
+  attr_reader :robots, :size
 
-	attr_reader :robots
+  def initialize
+    @size = SIZE
+    @robots = []
+  end
 
-	def initialize
-		@width = SIZE
-		@height = SIZE
-		@robots = []
-	end
+  def add_robot(robot)
+    return if find_robot(name: robot.name)
 
-	def add_robot(robot)
-		return if find_robot(name: robot.name)
-		@robots << robot
-	end
+    @robots << robot
+  end
 
-	def exisiting_robot?(x:, y:)
-		@robots.any? do |robot|
-			robot.x == x && robot.y == y
-		end
-	end
+  def exisiting_robot?(x:, y:)
+    @robots.any? do |robot|
+      robot.x == x && robot.y == y
+    end
+  end
 
-	def find_robot(name:)
-		@robots.detect do |robot|
-			robot.name == name
-		end
-	end
+  def find_robot(name:)
+    @robots.detect do |robot|
+      robot.name == name
+    end
+  end
 end
--- spec/model/board_spec.rb
+++ spec/model/board_spec.rb
@@ -1,26 +1,27 @@
-require "model/board"
-require "model/robot"
+# frozen_string_literal: true
+
+require 'model/board'
+require 'model/robot'
 
 RSpec.describe Board do
   let(:max_board_size) { 6 }
   let(:name) { 'Alice' }
-  describe "#add_robot" do
-  	let(:x) { 1 }
-  	let(:y) { 1 }
-  	let(:direction) { 'EAST' }
+  describe '#add_robot' do
+    let(:x) { 1 }
+    let(:y) { 1 }
+    let(:direction) { 'EAST' }
     let(:robot) { Robot.new(name: name, x: x, y: y, direction: direction, max_board_size: max_board_size) }
     let(:board) { described_class.new }
 
-    before do 
+    before do
       board.add_robot(robot)
     end
 
-    it "should add a new robot" do
+    it 'should add a new robot' do
       expect(board.robots).to eq([robot])
     end
 
     context 'when there is a same robot' do
-
       it 'should not add the duplicate robot' do
         board.add_robot(robot)
         expect(board.robots).to eq([robot])
@@ -29,14 +30,14 @@ RSpec.describe Board do
     end
   end
 
-  describe "#existing_robot" do
+  describe '#existing_robot' do
     context 'when there is no existing robot' do
       let(:x) { 1 }
       let(:y) { 1 }
       let(:board) { described_class.new }
-      
-      subject { board.exisiting_robot?(x: x, y: y)}
-      it "should return false" do
+
+      subject { board.exisiting_robot?(x: x, y: y) }
+      it 'should return false' do
         expect(subject).to eq(false)
       end
     end
@@ -47,13 +48,13 @@ RSpec.describe Board do
       let(:direction) { 'EAST' }
       let(:robot) { Robot.new(name: name, x: x, y: y, direction: direction, max_board_size: max_board_size) }
       let(:board) { described_class.new }
-      
+
       before do
         board.add_robot(robot)
       end
 
-      subject { board.exisiting_robot?(x: x, y: y)}
-      it "should return true" do
+      subject { board.exisiting_robot?(x: x, y: y) }
+      it 'should return true' do
         expect(subject).to eq(true)
       end
     end
@@ -66,11 +67,11 @@ RSpec.describe Board do
     let(:robot) { Robot.new(name: name, x: x, y: y, direction: direction, max_board_size: max_board_size) }
     let(:board) { described_class.new }
 
-    before do 
+    before do
       board.add_robot(robot)
     end
 
-    it "should add a new robot" do
+    it 'should add a new robot' do
       expect(board.find_robot(name: name)).to eq(robot)
     end
   end
